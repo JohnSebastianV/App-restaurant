3cd61df6f4dd00cf7d4729f1a64f5fc35c6ee1a6
JohnVillegas <villegas344@hotmail.com>
2025-08-25 02:03:30 -0500

    Se corrigue conftest

--- test/conftest.py
+++ test/conftest.py
@@ -2,7 +2,6 @@ import sys
 import os
 import pytest
 
-# Añadir carpeta raíz al path
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
 from app import create_app
@@ -11,13 +10,10 @@ from app import create_app
 def app():
     app = create_app()
     app.config["TESTING"] = True
-    # Otros settings si quieres, por ejemplo:
-    # app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///:memory:"
     yield app
 
 @pytest.fixture
 def client(app):
-    # Activar contexto de aplicación
     with app.app_context():
         with app.test_client() as client:
             yield client
